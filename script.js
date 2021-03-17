d5694662a08d36af4ebfa68d0b25cee206162788
dp160610 <46530061+dp160610@users.noreply.github.com>
2021-03-17 11:50:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-    $.get(
-      "https://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D",
-      function (data, status) {
-        for (let i = 0; i < data.length; i++) {
-          getData(data[i]);
-        }
+   $.get(
+    "https://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D",
+    function (data, status) {
+      for (let i = 0; i < data.length; i++) {
+        getData(data[i]);
       }
-    );
+    }
+  );
   
     let getData = (data) => {
       // let row = $('<tr class="data-row"></tr>')
